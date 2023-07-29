9d7e835b8d2bf6ee5456ec4fdc822b5cc85d5e46
mrami <si.mohamedrami1902@gmail.com>
2023-07-29 14:37:44 -0400

    last editing

--- src/App.js
+++ src/App.js
@@ -9,30 +9,24 @@ import Onlinefriend from "./pages/Onlinefriends/Onlinefriend";
 import Chats from "./pages/Chats/Chats";
 import Addfriends from "./pages/Addfriends/Addfriends";
 import Notification from "./pages/Notification/Notification";
-import Topbar from "./components/topbar/Topbar";
-import Sidebar from "./components/sidebar/Sidebar";
 
 
 function App() {
-  return (
-    <>
-    {/* <Topbar/>
-    <div className="app-container">
-    // <Sidebar/> */}
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/profile" element={<Profile/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/register" element={<Register/>}/>
-      <Route path="/videos" element={<Videos/>}/>
-      <Route path="/onlinefriend" element={<Onlinefriend/>}/>
-      <Route path="/chats" element={<Chats/>}/>
-      <Route path="/friend-request" element={<Addfriends/>}/>
-      <Route path="/notification" element={<Notification/>}/>
-    </Routes>
-    {/* </div> */}
-    </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/profile" element={<Profile />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/videos" element={<Videos />} />
+				<Route path="/onlinefriend" element={<Onlinefriend />} />
+				<Route path="/chats" element={<Chats />} />
+				<Route path="/friend-request" element={<Addfriends />} />
+				<Route path="/notification" element={<Notification />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
